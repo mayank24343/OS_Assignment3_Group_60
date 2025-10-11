fa3361ccd28e7195e95e78cab63cc8d0957b6f23
Mayank Yadav <mayank24343@iiitd.ac.in>
2025-10-11 13:47:34 +0000

    Initialised Shared Memory & Semaphore, Worked on SimpleShell

--- SimpleScheduler.c
+++ SimpleScheduler.c
@@ -8,20 +8,61 @@
 #include <fcntl.h>
 #include <time.h>
 
+typedef struct{
+        char command[1024];
+        pid_t pid;
+        unsigned int tslices_on_cpu;
+        unsigned int tslices_off_cpu;
+        int state;
+        //state is 0: ready 1: running 2: finished
+} Process;
+
+typedef struct{
+        Process table[256];
+        int count;
+        sem_t* sem;
+} SharedProcessTable;
+
+SharedProcessTable* processtable;
+int shm_fd;
 //how the queue will work
 //maintain like a pointer to start of queue & the current pointer
 //current pointer moves to give the next ncpu processes with state ready
 //if ncpu not found till end of table, it loops back to the start using the start pointer
 //the first time u encounter an already running process - means u have seen all the processes in the table - so stop now
+
 //signal handler required, which handles SIGINT: will wait for the processes to finish, display information & then exit
+static void my_handler(int signal){
+}
+
+void setup(){
+	shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+	processtable = mmap(NULL, sizeof(SharedProcessTable), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (processtable == MAP_FAILED){
+		perror("mmap failed");
+		exit(1);
+	}
+}
+
+void cleanup(){
+	munmap(processtable,sizeof(SharedProcessTable));
+	close(shm_fd);
+}
 
 int main(int argc, char** argv){
 	//sigint signal handler
+	struct sigaction sig;
+	memset(&sig,0,sizeof(sig));
+	sig.sa_handler = my_handler;
+	sigaction(SIGINT,&sig,NULL);
+
 	//access the shared memory which has the process table
+	setup();
 	while (1){
 		//use semaphore, read the process table to find the next ncpu processes to run as per round robin policy, send SIGCONT to these for running & set state = 1 (running) in table
 		//nano sleep for tslice miliseconds
 		//use semaphore, read the ncpu running processes, send SIGSTOP to pause, set state to ready
 	}
+	cleanup();
 	return 0;
 }
--- SimpleShell.c
+++ SimpleShell.c
@@ -13,12 +13,20 @@
 
 typedef struct{
 	char command[1024];
-	pid_t* pid;
+	pid_t pid;
 	unsigned int tslices_on_cpu;
 	unsigned int tslices_off_cpu;
 	int state;
 	//state is 0: ready 1: running 2: finished
 } Process;
+
+typedef struct{
+	Process table[256];
+	int count;
+	sem_t* sem;
+} SharedProcessTable;
+
+SharedProcessTable* processtable;
 
 char** read_command(char* command){
 	//take string command and return tokens (space separated words)
@@ -40,18 +48,89 @@ char** read_command(char* command){
 	return args;
 }
 
+static void my_handler(int signal){
+	int status;
+	pid_t pid;
+	while ((pid = waitpid(-1,&status,WNOHANG)) > 0){
+		//wait to get the pid of terminated child without blocking (WNOHANG)
+		sem_wait(processtable->sem);//using shared memory
+		for(int i = 0; i < processtable->count; i++){
+			if (processtable->table[i].pid == pid){
+				processtable->table[i].state = 2; //finishde
+			}
+		}
+		sem_post(processtable->sem);
+	}
+}
+
 void submit(char* command){
 	char** args = read_command(command);//read arguments
 	if (strcmp(args[0],"submit") == 0){
 		printf("%s",args[1]);
 		//executable submitted
 		//fork to create child process & execute - execution pauses due to dummy main calling raise(SIGSTOP)
-		//parent adds process to process table in shared memory using semaphores
+		pid_t child = fork();
+		if (child == 0){
+			//in child process
+			execvp(args[1],++args);//++args to skip the submit word
+			perror("exec failed");
+			exit(1);
+		}
+		else if (child > 0){
+			//in parent
+			sem_wait(processtable->sem);//modifying in shm
+			if (processtable->count < 256){
+				Process* p = &(ptable->table[count]);
+				p->pid = child;
+				p->tslices_on_cpu = 0;
+				p->tslices_off_cpu = 0;
+				p->state = 0; //running
+				strcpy(p->command,args[1]);
+			//parent adds process to process table in shared memory using semaphores
+			}
+			else{
+				printf("Process table full, cannot execute any more processes!!");
+			}
+			sem_post(processtable->sem);
 	}
 	else{
 		//shell need not handle other commands for this assignment
 		return;
 	}
+}
+
+void display_info(){
+	//to display process name, pid, wait time = no of tslices off cpu * tslice, completion time = tslices_on_cpu+tslices_off_cpu*tslice
+}
+
+pid_t scheduler_pid;
+int shm_fd;
+
+void setup_shm_sem(){
+	//shm setup
+	shm_fd = shm_open("/processtable", O_CREAT | O_RDWR, 0666);
+	if (shm_fd < 0){
+		perror("shm_open failed");
+		exit(1);
+	}
+	ftruncate(shm_fd, sizeof(SharedProcessTable);
+	processtable = mmap(NULL, sizeof(SharedProcessTable), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (processtable == MAP_FAILED){
+		perror("mmap failed");
+		exit(1);
+	}
+	processtable->count = 0;
+
+	//semaphore setup
+	sem_init(processtable->sem, 1, 1);
+}
+
+void cleanup(){
+	//cleanup semaphore, shared memry
+	sem_destroy(processtable->sem);
+	munmap(processtable, sizeof(SharedProcessTable));
+	close(shm_fd);
+	shm_unlink("/processtable");
 }
 
 void shell_loop(){
@@ -61,6 +140,9 @@ void shell_loop(){
 		fgets(command,1024,stdin);
 		if (strcmp(command,"exit\n") == 0){
 			//terminate scheduler by sending sigint (scheduler has a sigint handler to wait for each process to end, display details & then exit)
+			kill(scheduler_pid, SIGINT);
+			waitpid(scheduler_pid,NULL,0);
+			display_info();
 			//exit the shell
 			break;
 		}
@@ -84,8 +166,25 @@ int main(int argc,char** argv){
 		exit(1);
 	}
 	//initialise a shared memory for storing the process table (linked list) & sempaphore for mutual exclusion while modifying the process table
-	//start a process to start execution of simple scheduler
+	setup_shm_sem();
+	struct sigaction sig;
+	memset(&sig,0,sizeof(sig));
+	sig.sa_handler = my_handler;
+	sigaction(SIGCHLD,&sig,NULL);
 	//also need a signal handler to handle SIGCHLD by the submitted process when they end, need to update process table & mark them as finished
+
+	//process for shceduler
+	scheduler_pid = fork();
+	if (scheduler_pid == 0){
+		//execute scheduler here using exec family
+		perror("exec failed");
+		exit(1);
+	}
+	else if (scheduler_pid < 0){
+		perror("fork failed");
+		exit(1);
+	}
 	shell_loop();
+	cleanup();
 	return 0;
 }
